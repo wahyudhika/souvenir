eadb40dca6005de64f0c556e1e7e124be67c604b
wahyudhika <50437798+wahyudhika@users.noreply.github.com>
2019-11-05 13:18:10 +0700

    Update index.php

--- souvenir_bkt/index.php
+++ souvenir_bkt/index.php
@@ -38,7 +38,7 @@ require("../connect.php");
 
 <script type="text/javascript">
 
-var server = "http://localhost:8080/souvenir_postgre/souvenir_bkt/";
+var server = "http://13.76.103.39/bkt_souvenir/souvenir_bkt/";
 var map;
 var markersDua = [];
 var koordinat = 'null'
