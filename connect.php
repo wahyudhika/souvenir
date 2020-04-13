0b20f6c6b0e7b9831627abee792ed69eff012826
wahyudhika <50437798+wahyudhika@users.noreply.github.com>
2020-04-13 17:06:19 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 	$host = "localhost";
 	$user = "postgres";
 	$pass = "wahyu123";
-	$port = "5432";
+	$port = "5433";
 	$dbname = "bkt_souvenir";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
